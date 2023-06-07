37721b576a2ef71a5e461f8379ee0bb7affd0f3f
Adam Oberle <oberle.adam@gmail.com>
2023-06-06 21:13:52 -0500

    despite many moments of panic, base mode complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,54 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const [newAirline, setNewAirline] = useState('');
+  const airlineList = useSelector(store => store.airlineList)
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch({
+      type: 'ADD_AIRLINE',
+      payload: newAirline
+    });
+
+    setNewAirline('');
+  }
+
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder='Airline Name' 
+        onChange={event => setNewAirline(event.target.value)}
+        value={newAirline}
+        />
+        <button type="submit">Add Airline</button>
+      </form>
+      <br></br>
+      <table>{/* Airlines should be listed here */}
+        <thead>
+          <tr>
+            <th>Airlines</th>
+          </tr>
+        </thead>
+        <tbody>
+          {airlineList.map((airline, i) => (
+            <tr key={i}>
+              <td>{airline}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
